ee11f43321c40ff835053d00b8d03f0004904ce8
advin kautsar <advinkautsar07@gmail.com>
2020-10-14 12:53:21 +0700

    commit2

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,7 +6,7 @@
             <div class="col-sm-4">
                 <div class="page-header float-left">
                     <div class="page-title">
-                        <h1>Dashboard</h1>
+                        <h1>Beranda</h1>
                     </div>
                 </div>
             </div>
